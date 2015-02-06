78517c36aa5d137d32d8a836a3c1ceb3fd15dfc4
Spike Brehm <ocelot@gmail.com>
2015-02-06 15:27:51 -0800

    Initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,12 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+//= require jquery
+//= require underscore.js
+//= require backbone.js
+//= require backbone.localStorage.js
+//= require handlebars.runtime.js
+//= require_tree ./templates
+//= require models/todo.js
+//= require collections/todos.js
+//= require_tree .
---
+++ test/controllers/home_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HomeControllerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
